09c1d10d553463a8004824e204563bdd1b2242b3
make <280719844@qq.com>
2023-05-18 15:14:00 +0800

    test

--- lesson2/apps/b0/src/initcalls.c
+++ lesson2/apps/b0/src/initcalls.c
@@ -1,11 +1,36 @@
 /* Helper for getting init_calls_[start/end] */
 
-u64 initcalls_start() {
+/*
+.text : ALIGN(4K) {
+        stext = .;
+        *(.text.boot)
+        *(.text .text.*)
+        . = ALIGN(4K);
+        etext = .;
+        init_calls_start = .;
+        KEEP(*(.init_calls))
+        init_calls_end = .;
+    }
+*/
+
+// 本来想直接使用上面init_calls_start的符号， 其实要使用init_calls_start[]才行;
+// 直接使用init_calls_end 会发现值为0， 由于所有名为 .init_calls 的段都被合并到了最终的可执行文件中，
+// 所以最终的可执行文件的 .init_calls 段结束位置为 0，
+// 即所有 .init_calls 段的总大小为 0。因此，在链接脚本中，init_calls_end 的位置也被设置为 0。
+
+extern void* init_calls_start[];
+extern void* init_calls_end[];
+
+#define __READ_ONCE(x) (*(const volatile unsigned long int *) & (x))
+
+unsigned long int initcalls_start() {
     /* Todo: fix it! */
-    return 0;
+    unsigned long int address =  (unsigned long int)init_calls_start;
+    return __READ_ONCE(address);
 }
 
-u64 initcalls_end() {
+unsigned long int initcalls_end() {
     /* Todo: fix it! */
-    return 0;
+    unsigned long int address =  (unsigned long int)init_calls_end;
+    return __READ_ONCE(address);
 }
--- lesson2/apps/b0/src/main.rs
+++ lesson2/apps/b0/src/main.rs
@@ -5,6 +5,33 @@ use drv0 as _;
 use drv1 as _;
 
 use drv_common::CallEntry;
+// use std::mem;
+
+extern "C" {
+    fn initcalls_start() -> u64;
+    fn initcalls_end() -> u64;
+}
+
+// 主要被卡住的地方就是链接期间找不到 __stack_chk_guard 和 __stack_chk_fail 这两个符号
+// 刚开始把重心都放到了initcalls.c中， 一直都以为c程序出错了(也许c程序也是错的)
+
+// 下面手动添加这些符号
+#[no_mangle]
+pub extern "C" fn __stack_chk_fail() {}
+
+#[no_mangle]
+pub static mut __stack_chk_guard: u64 = 0;
+
+
+fn get_initcalls_start() -> u64 {
+    let start: u64 = unsafe { initcalls_start() };
+    start
+}
+
+fn get_initcalls_end() -> u64 {
+    let end: u64 = unsafe { initcalls_end() };
+    end
+}
 
 #[no_mangle]
 fn main() {
@@ -16,12 +43,25 @@ fn main() {
 
 /* Todo: Implement it */
 fn traverse_drivers() {
-    libos::println!("\n!!! Fix it !!!\n");
+    // libos::println!("\n!!! Fix it !!!\n");
     // Parse range of init_calls by calling C function.
-    // display_initcalls_range(range_start, range_end);
+
+    let range_start: u64 = get_initcalls_start();
+    let range_end: u64 = get_initcalls_end();
+
+    display_initcalls_range(range_start as usize, range_end as usize);
 
     // For each driver, display name & compatible
-    // display_drv_info(drv.name, drv.compatible);
+    let mut offset = 0;
+    while range_start + offset < range_end {
+        let call_entry = (range_start + offset) as *const CallEntry;
+        let drv;
+        unsafe {
+            drv = ((*call_entry).init_fn)();
+        }
+        display_drv_info(drv.name, drv.compatible);
+        offset += 8;  // size of call entry
+    }
 }
 
 fn display_initcalls_range(start: usize, end: usize) {
